453c3542212cd158490df8b02eea303df87c8c23
chitwang12 <chitwangandhi12@gmail.com>
2023-09-10 15:05:55 +0530

    DOM intro

---
+++ JSLearnngsCN/DOM/dom.js
@@ -0,0 +1,7 @@
+var heading = document.getElementById('one');
+heading.style.color = "red";
+heading.style.backgroundColor='Cyan';
+
+var headingsh1= document.getElementsByTagName('h1');
+headingsh1[1].innerText ="Second Headdiiing"
+console.log(headingsh1);
