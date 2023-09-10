4f3775a8e3a1ccdd3f957cc4b8fb02c83066d4ae
chitwang12 <chitwangandhi12@gmail.com>
2023-09-10 18:09:22 +0530

    Objects and Timming Events Brief

---
+++ JSLearnngsCN/Objects&TimmingsEvent/timerTask.js
@@ -0,0 +1,11 @@
+function timer() {
+    for(let i = 1; i<=5; i++){
+        setTimeout(function() {
+           //Write your code here...
+          console.log(i)
+        }, //write your code here...);
+        
+    }
+    console.log("Hello ninjas!");
+}
+timer();
